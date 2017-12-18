560e99425b68fe09717b5d412fb21de05ea6e638
余晓冬 <yuxiaodong@xianglin.cn>
2017-12-18 20:21:23 +0800

    JLJ

--- feiji.py
+++ feiji.py
@@ -3,16 +3,25 @@ import pygame
 from pygame.locals import *
 import time
 import random
+from PIL import Image
 
 class Plane(object):
-    def __init__(self, screen, x, y, image_path):
+    def __init__(self, screen, x, y, image_path, down_imgs):
         self.x = x
         self.y = y
         self.image = pygame.image.load(image_path)
+        self.blowups = []
+        for down_img in down_imgs:
+            self.blowups.append(pygame.image.load(down_img))
+
+        fp = open(image_path, 'r')
+        img = Image.open(fp)
+        fp.close()
+        self.size = (img.size[0], img.size[1])
         self.screen = screen
         self.bullet_list = []
 
-    def display(self):
+    def display(self, plane):
         self.screen.blit(self.image, (self.x, self.y))
 
         for bullet in self. bullet_list:
@@ -20,6 +29,37 @@ class Plane(object):
             bullet.move()
             if bullet.judge():
                 self.bullet_list.remove(bullet)
+            if type(plane) != list:
+                if self.hit(plane, bullet):
+                    plane.crash()
+                    if self.bullet_list:
+                        self.bullet_list.remove(bullet)
+            else:
+                for p in plane:
+                    if self.hit(p, bullet):
+                        if self.bullet_list:
+                            self.bullet_list.remove(bullet)
+                        p.crash()
+
+                        plane.remove(p)
+
+        if type(plane) != list:
+            if self.pang(plane):
+                plane.crash()
+        else:
+            for p in plane:
+                if self.pang(p):
+                    p.crash()
+                    plane.remove(p)
+                    self.crash()
+
+
+
+    def pang(self, plane):
+        if (plane.y + plane.size[1] > self.y and plane.y < self.y + self.size[1] and plane.x + plane.size[0] > self.x and plane.x < self.x + self.size[0]):
+            return True
+        else:
+            return False
 
     def move_left(self):
         self.x -= 10
@@ -39,7 +79,26 @@ class Plane(object):
         #     self.x -= random.randint(10, 200)
 
     def fire(self):
-        self.bullet_list.append(Bullet(self.screen, self.x, self.y))
+        self.bullet_list.append(Bullet(self, './feiji/bullet.png'))
+
+    def judge(self):
+        if self.y > 600:
+            return True
+        else:
+            return False
+
+    def hit(self, plane, bullet):
+
+        if plane.y + plane.size[1] > 0 and (bullet.y < plane.y + plane.size[1] and bullet.y + bullet.size[1] > plane.y) and (bullet.x + bullet.size[0] > plane.x and bullet.x < plane.x + plane.size[0]):
+            return True
+        else:
+            return False
+
+
+    def crash(self):
+        for blowup in self.blowups:
+            self.screen.blit(blowup, (self.x, self.y))
+
 
 class PlaneHero(Plane):
     pass
@@ -48,17 +107,29 @@ class Enemy(Plane):
     def fire(self):
         random_num = random.randint(1, 100)
         if random_num == 8 or random_num == 78:
-            self.bullet_list.append(EnemyBullet(self.screen, self.x, self.y))
+            self.bullet_list.append(EnemyBullet(self, './feiji/bullet1.png'))
+
+    def hit(self, plane, bullet):
+
+        if (bullet.y + bullet.size[1] > plane.y and bullet.y < plane.y + plane.size[1]) and (bullet.x + bullet.size[0] > plane.x and bullet.x < plane.x + plane.size[0]):
+            return True
+        else:
+            return False
+
 
 
 
 
 class Bullet(object):
-    def __init__(self, screen, plane_x, plane_y):
-        self.x = plane_x + 50 - 11
-        self.y = plane_y - 22
-        self.image = pygame.image.load('./feiji/bullet.png')
-        self.screen = screen
+    def __init__(self, plane, image_path):
+        fp = open(image_path, 'r')
+        img = Image.open(fp)
+        fp.close()
+        self.size = img.size
+        self.x = plane.x + plane.size[0] / 2 - self.size[0] /2
+        self.y = plane.y - img.size[1]
+        self.image = pygame.image.load(image_path)
+        self.screen = plane.screen
 
     def display(self):
         self.screen.blit(self.image, (self.x, self.y))
@@ -67,17 +138,21 @@ class Bullet(object):
         self.y -= 5
 
     def judge(self):
-        if self.y < 0:
+        if self.y < 0 - self.size[1]:
             return True
         else:
             return False
 
-class EnemyBullet(object):
-    def __init__(self, screen, plane_x, plane_y):
-        self.x = plane_x + 34.5 - 4.5
-        self.y = plane_y + 89
-        self.image = pygame.image.load('./feiji/bullet1.png')
-        self.screen = screen
+class EnemyBullet(Bullet):
+    def __init__(self, plane, image_path):
+        fp = open(image_path, 'r')
+        img = Image.open(fp)
+        fp.close()
+        self.size = img.size
+        self.x = plane.x + plane.size[0] / 2 - self.size[0] /2
+        self.y = plane.y + plane.size[1]
+        self.image = pygame.image.load(image_path)
+        self.screen = plane.screen
 
     def display(self):
         self.screen.blit(self.image, (self.x, self.y))
@@ -90,6 +165,43 @@ class EnemyBullet(object):
             return True
         else:
             return False
+
+class Screen(object):
+    def __init__(self, image_path):
+        self.enemy_list = []
+        self.background = pygame.image.load(image_path)
+        self.screen = pygame.display.set_mode((480, 600), 0, 32)
+
+
+
+    def add_enemy(self):
+        random_num = random.randint(1, 100)
+        if random_num == 8 or random_num == 78:
+            self.enemy_list.append(Enemy(self.screen, random.randint(0, 400), -100, './feiji/enemy1.png', ('./feiji/enemy1_down1.png', './feiji/enemy1_down2.png', './feiji/enemy1_down3.png', './feiji/enemy1_down4.png')))
+
+    def display(self):
+        hero = PlaneHero(self.screen, 190, 472, './feiji/hero1.png', ('./feiji/hero_blowup_n1.png', './feiji/hero_blowup_n2.png', './feiji/hero_blowup_n3.png', './feiji/hero_blowup_n4.png'))
+        pygame.key.set_repeat(10)
+        while True:
+            self.screen.blit(self.background, (0, 0))
+            hero.display(self.enemy_list)
+            for enemy in self.enemy_list:
+                enemy.display(hero)
+                enemy.auto_move()
+                enemy.fire()
+                if enemy.judge():
+                    self.enemy_list.remove(enemy)
+            self.add_enemy()
+            pygame.display.update()
+            key_control(hero)
+            key_space(hero)
+            time.sleep(0.01)
+
+
+
+
+
+
 
 
 def key_control(hero):
@@ -116,26 +228,14 @@ def key_space(hero):
         hero.fire()
 
 def main():
-    screen = pygame.display.set_mode((480,600),0,32)
-    background = pygame.image.load('./feiji/background.png')
-    hero = PlaneHero(screen, 190, 472, './feiji/hero1.png')
-    bullet = Bullet(screen,hero.x, hero.y)
-    enemy = Enemy(screen, 0, 0, './feiji/enemy1.png')
-    pygame.key.set_repeat(10)
-    while True:
-        screen.blit(background, (0, 0))
-        hero.display()
-        enemy.display()
-        enemy.auto_move()
-        enemy.fire()
-        # bullet.display()
-        pygame.display.update()
-        key_control(hero)
-        key_space(hero)
-
-                    # pygame.display.update()
+    screen = Screen('./feiji/background.png')
 
 
-        time.sleep(0.01)
+    screen.display()
+
+
+
+
+
 if __name__ == "__main__":
     main()
